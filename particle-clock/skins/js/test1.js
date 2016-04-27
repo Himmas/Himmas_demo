8994ecec3b58102225c2c8d5af14fceedbd2836f
chyuan <chyuan@dev.4dq.com>
2016-04-27 21:02:14 +0800

    doge_计时器卡顿修复

--- particle-clock/skins/js/test1.js
+++ particle-clock/skins/js/test1.js
@@ -3,62 +3,80 @@ var windows_width=1024,
 	radius=8,
 	round_top=60,
 	round_left=30,
-	minutesShowTimeSeconds=0;
-const endTime=new Date('2016/3/4,13:13:13');
+	minutesShowTimeSeconds=0,
+	curShowTimeSeconds=0;
+//const endTime=new Date('2016/3/4,13:13:13');
 var balls = [];
 const colors = ["#33B5E5","#0099CC","#AA66CC","#9933CC","#99CC00","#669900","#FFBB33","#FF8800","#FF4444","#CC0000"];
 
 //主函数
 window.onload=function test1(){
-	windows_width = $(document.body).width();
-    windows_height = $(window).height();
+	windows_width = $(window).width()-40;
+    windows_height = $(window).height()-40;
 	$("#canvas").attr("width",windows_width);
 	$("#canvas").attr("height",windows_height);
 	round_left = Math.round(windows_width /10);
 	round_top = Math.round(windows_height /5);
-	radius = Math.round(windows_width * 4 / 5 / 108)-1
-	console.log("20",windows_height);
+	radius = Math.round(windows_width * 4 / 5 / 108)-1;
 	var _canvas=document.getElementById('canvas').getContext("2d");
+	
+	curShowTimeSeconds = getShowTime();
+	
 	setInterval(function(){
-		updateBalls();
+//		updateBalls();
 		randerTest(_canvas);
+		update();
     },50);
-    setInterval(function(){
-        update();
-    },1000);
+//  setInterval(function(){
+//      update();
+//  },1000);
 //  圆角矩形背景调用方法(画布清除方法冲突)
 //  saveRoundRect(_canvas,0,0,windows_width,windows_height,50);
 }
 
+function getShowTime(){
+	var test_time=new Date();
+	var ret = test_time.getHours() * 3600 + test_time.getMinutes() * 60 + test_time.getSeconds();
+	console.log("40",ret);
+	return ret;
+}
+
 //更新时间&&判断更新部分添加小球方法
 function update(){
-	var test1_time=new Date();
-	var hours=test1_time.getHours();
-	var minutes=test1_time.getMinutes();
-	var seconds=test1_time.getSeconds();
+//	var test_time=new Date();
+	var newShowTime=getShowTime();
 	
-	var now = new Date(test1_time.getTime() - 1000);
-	var now_hours=now.getHours();
-	var now_minutes=now.getMinutes();
-	var now_seconds=now.getSeconds();
-	if( parseInt(hours/10)!=parseInt(now_hours/10)){
-        addBalls( round_left + 0 , round_top , parseInt(hours/10) );
+	var hours=parseInt( newShowTime / 3600);
+	var minutes=parseInt( (newShowTime - hours * 3600)/60 );
+	var seconds=newShowTime % 60;
+	
+	var now_hours=parseInt( curShowTimeSeconds / 3600);
+	var now_minutes=parseInt( (curShowTimeSeconds - now_hours * 3600)/60 );
+	var now_seconds=curShowTimeSeconds % 60;
+	
+	if(seconds!=now_seconds){
+		if( parseInt(hours/10)!=parseInt(now_hours/10)){
+	        addBalls( round_left + 0 , round_top , parseInt(hours/10) );
+	    }
+	    if( parseInt(hours%10)!=parseInt(now_hours%10)){
+	        addBalls( round_left + 15*(radius+1) , round_top , parseInt(hours%10) );
+	    }
+	    if( parseInt(minutes/10)!=parseInt(now_minutes/10)){
+	        addBalls( round_left + 40*(radius+1) , round_top , parseInt(minutes/10) );
+	    }
+		if( parseInt(minutes%10)!=parseInt(now_minutes%10)){
+		    addBalls( round_left + 55*(radius+1) , round_top , parseInt(minutes%10) );
+		}
+	    if( parseInt(seconds/10)!=parseInt(now_seconds/10)){
+	        addBalls( round_left + 80*(radius+1) , round_top , parseInt(seconds/10) );
+	    }
+	    if( parseInt(seconds%10)!=parseInt(now_seconds%10)){
+	        addBalls( round_left + 95*(radius+1) , round_top , parseInt(seconds%10) );
+	    }
+	    curShowTimeSeconds = newShowTime;
     }
-    if( parseInt(hours%10)!=parseInt(now_hours%10)){
-        addBalls( round_left + 15*(radius+1) , round_top , parseInt(hours%10) );
-    }
-    if( parseInt(minutes/10)!=parseInt(now_minutes/10)){
-        addBalls( round_left + 40*(radius+1) , round_top , parseInt(minutes/10) );
-    }
-	if( parseInt(minutes%10)!=parseInt(now_minutes%10)){
-	    addBalls( round_left + 55*(radius+1) , round_top , parseInt(minutes%10) );
-	}
-    if( parseInt(seconds/10)!=parseInt(now_seconds/10)){
-        addBalls( round_left + 80*(radius+1) , round_top , parseInt(seconds/10) );
-    }
-    if( parseInt(seconds%10)!=parseInt(now_seconds%10)){
-        addBalls( round_left + 95*(radius+1) , round_top , parseInt(seconds%10) );
-    }
+    updateBalls();
+    console.log( balls.length)
 }
 
 //添加小球函数
@@ -104,10 +122,10 @@ function updateBalls(){
 //画布上显示当前时间(基础显示)
 function randerTest(cxt){
 	cxt.clearRect(0,0,windows_width, windows_height);
-	var test1_time=new Date();
-	var hours=test1_time.getHours();
-	var minutes=test1_time.getMinutes();
-	var seconds=test1_time.getSeconds();
+//	var test_time=new Date();
+	var hours=parseInt( curShowTimeSeconds / 3600);
+	var minutes=parseInt( (curShowTimeSeconds - hours * 3600)/60 );
+	var seconds=curShowTimeSeconds % 60;
 	
 	renderDigit(round_left,round_top,parseInt(hours/10),cxt);
     renderDigit(round_left + 15*(radius+1),round_top,parseInt(hours%10) ,cxt);
