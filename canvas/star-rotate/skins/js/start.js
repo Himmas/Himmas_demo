02509d55d7fc66e07adae7699e82e5275fe27b53
saltleaves <806450659@qq.com>
2016-05-29 22:47:13 +0800

    小doge的星星

--- canvas/star-rotate/skins/js/start.js
+++ canvas/star-rotate/skins/js/start.js
@@ -1,6 +1,9 @@
 var _scorll=18;
 var _width=600;
 var _height=600;
+var arr_r = [];
+var arr_x = [];
+var arr_y = [];
 //主函数
 window.onload=function(){
 	var canvas=document.getElementById("canvas");
@@ -20,25 +23,30 @@ window.onload=function(){
 	context.fillRect(0,0,canvas.width,canvas.height);
 	
 //	星星单循环部分
+
 	for(var j=0;j<10;j++){
 		var _r=Math.random()*10+10;
 		var _R=_r*2;
 		var _x=Math.random()*canvas.width;
 		var _y=Math.random()*canvas.height;
 //		var _sor=Math.random()*360;
-		
-		var draw=drawStar(context,_r,_R,_x,_y,_scorll);
-		
-		setInterval(function(){		
-			_scorll=18*i;
-			i++;
-			context.save();
-			drawStar(context,_r,_R,canvas.width/2,canvas.height/2,_scorll);
-			context.restore();
-		},80);
+		var draw=drawStar(context,_r,_R,_x,_y,_scorll);	
+		arr_r.push(_r);
+		arr_x.push(_x);
+		arr_y.push(_y);
 	}
-//	console.log(_scorll,_r,_R,_x,_y);
+// 	console.log(_scorll,_r,_R,_x,_y);
+	setInterval(function(){	
+	for(var j=0;j<10;j++){
+		_scorll=18*i;
+		i++;
+		context.save();
+		drawStar(context,arr_r[j],arr_r[j]*2,arr_x[j],arr_y[j],_scorll);
+		context.restore();
+	}			
+	},80);
 }
+
 //五角星计算函数
 function drawStar(cxt,r,R,x,y,sor){
 //	cxt.clearRect(0,0,_width, _height);/*清除指定画布区域*/
@@ -55,8 +63,6 @@ function drawStar(cxt,r,R,x,y,sor){
 		);
 	}
 	cxt.closePath();
-	
-	
 	cxt.fillStyle="#4087ce";
 	cxt.strokeStyle="#e0e0e0";
 	cxt.lineWidth=5;
