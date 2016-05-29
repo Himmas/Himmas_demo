38ec1744084cb46ade13778038c409e0d5cad5a9
saltleaves <806450659@qq.com>
2016-05-29 23:05:19 +0800

    改的更正常点

--- canvas/star-rotate/skins/js/start.js
+++ canvas/star-rotate/skins/js/start.js
@@ -13,14 +13,14 @@ window.onload=function(){
 	var i=1;
 	
 //	径向渐变背景图片
-	var skyStyle=context.createRadialGradient(
-		canvas.width/2,canvas.height,0,
-		canvas.width/2,canvas.height,canvas.height
-	);
-	skyStyle.addColorStop(0.0,'#035');
-	skyStyle.addColorStop(1.0,'#000');
-	context.fillStyle=skyStyle;
-	context.fillRect(0,0,canvas.width,canvas.height);
+	// var skyStyle=context.createRadialGradient(
+	// 	canvas.width/2,canvas.height,0,
+	// 	canvas.width/2,canvas.height,canvas.height
+	// );
+	// skyStyle.addColorStop(0.0,'#035');
+	// skyStyle.addColorStop(1.0,'#000');
+	// context.fillStyle=skyStyle;
+	// context.fillRect(0,0,canvas.width,canvas.height);
 	
 //	星星单循环部分
 
@@ -37,20 +37,31 @@ window.onload=function(){
 	}
 // 	console.log(_scorll,_r,_R,_x,_y);
 	setInterval(function(){	
-	for(var j=0;j<10;j++){
-		_scorll=18*i;
-		i++;
-		context.save();
-		drawStar(context,arr_r[j],arr_r[j]*2,arr_x[j],arr_y[j],_scorll);
-		context.restore();
-	}			
+		context.clearRect(0,0,_width, _height);//清除指定画布区域
+		//	径向渐变背景图片
+		var skyStyle=context.createRadialGradient(
+		canvas.width/2,canvas.height,0,
+		canvas.width/2,canvas.height,canvas.height
+		);
+		skyStyle.addColorStop(0.0,'#035');
+		skyStyle.addColorStop(1.0,'#000');
+		context.fillStyle=skyStyle;
+		context.fillRect(0,0,canvas.width,canvas.height);
+		//循环区域
+		for(var j=0;j<10;j++){
+			_scorll=i*1.5;
+			i++;
+			context.save();
+			drawStar(context,arr_r[j],arr_r[j]*2,arr_x[j],arr_y[j],_scorll);
+			context.restore();
+		}			
 	},80);
 }
 
 //五角星计算函数
 function drawStar(cxt,r,R,x,y,sor){
-//	cxt.clearRect(0,0,_width, _height);/*清除指定画布区域*/
-//	cxt.clearRect(x,y,r,R);/*清除指定画布区域*/
+	// cxt.clearRect(0,0,_width, _height);清除指定画布区域
+	// cxt.clearRect(x,y,r,R);/*清除指定画布区域*/
 	cxt.beginPath();
 	for(var i=0;i<5;i++){
 		cxt.lineTo(
