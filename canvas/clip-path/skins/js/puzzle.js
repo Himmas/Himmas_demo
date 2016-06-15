32ca15a06f2d09623b9acf2dea56126cd1057b17
zsychy <984249214@qq.com>
2016-06-15 23:08:40 +0800

    Doge_日常打卡

--- canvas/clip-path/skins/js/puzzle.js
+++ canvas/clip-path/skins/js/puzzle.js
@@ -70,8 +70,75 @@ function btn_l(){
 	}else if(i==30){
 		i=1;
 		$("._puzzle").removeClass("drill").addClass("whale");
+		return;
 	}
 	i++;
-	console.log(i);
 }
+function btn_r(){
+	if(i==1){
+		$("._puzzle").removeClass("whale").addClass("drill");
+		i=30;
+		return;
+	}else if(i==30){
+		$("._puzzle").removeClass("drill").addClass("hirola");
+	}else if(i==29){
+		$("._puzzle").removeClass("hirola").addClass("loris");
+	}else if(i==28){
+		$("._puzzle").removeClass("loris").addClass("okapi");
+	}else if(i==27){
+		$("._puzzle").removeClass("okapi").addClass("peccary");
+	}else if(i==26){
+		$("._puzzle").removeClass("peccary").addClass("rhino");
+	}else if(i==25){
+		$("._puzzle").removeClass("rhino").addClass("lynx");
+	}else if(i==24){
+		$("._puzzle").removeClass("lynx").addClass("sifaka");
+	}else if(i==23){
+		$("._puzzle").removeClass("sifaka").addClass("tapir");
+	}else if(i==22){
+		$("._puzzle").removeClass("tapir").addClass("panda");
+	}else if(i==21){
+		$("._puzzle").removeClass("panda").addClass("ostrich");
+	}else if(i==20){
+		$("._puzzle").removeClass("ostrich").addClass("butterfly");
+	}else if(i==19){
+		$("._puzzle").removeClass("butterfly").addClass("camel");
+	}else if(i==18){
+		$("._puzzle").removeClass("camel").addClass("parrotfish");
+	}else if(i==17){
+		$("._puzzle").removeClass("parrotfish").addClass("bear");
+	}else if(i==16){
+		$("._puzzle").removeClass("bear").addClass("damselfly");
+	}else if(i==15){
+		$("._puzzle").removeClass("damselfly").addClass("penguin");
+	}else if(i==14){
+		$("._puzzle").removeClass("penguin").addClass("echidna");
+	}else if(i==13){
+		$("._puzzle").removeClass("echidna").addClass("kakapo");
+	}else if(i==12){
+		$("._puzzle").removeClass("kakapo").addClass("sloth");
+	}else if(i==11){
+		$("._puzzle").removeClass("sloth").addClass("armadillo");
+	}else if(i==10){
+		$("._puzzle").removeClass("armadillo").addClass("seahorse");
+	}else if(i==9){
+		$("._puzzle").removeClass("seahorse").addClass("iguana");
+	}else if(i==8){
+		$("._puzzle").removeClass("iguana").addClass("oryx");
+	}else if(i==7){
+		$("._puzzle").removeClass("oryx").addClass("turtle");
+	}else if(i==6){
+		$("._puzzle").removeClass("turtle").addClass("crow");
+	}else if(i==5){
+		$("._puzzle").removeClass("crow").addClass("owl");
+	}else if(i==4){
+		$("._puzzle").removeClass("owl").addClass("frog");
+	}else if(i==3){
+		$("._puzzle").removeClass("frog").addClass("tamarin");
+	}else if(i==2){
+		$("._puzzle").removeClass("tamarin").addClass("whale");
+	}
+	i--;
+}
+
 
